e025e8f1bf173d17d02213150a7ad9116b40a0cd
FA <finneganadair@gmail.com>
2026-06-04 10:33:35 -0400

    fixing

--- main.rs
+++ main.rs
@@ -21,7 +21,7 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Er
     let mut lines = buf_reader.lines();
     let request_line = lines.next()
         .ok_or("No request line")?
-        .map_err(|e| e.into())?;
+        .map_err(|e| <std::io::Error as Into<T>>::into(e))?;
     
     let http_request: Vec<_> = lines
         .map(|result| result.unwrap())
